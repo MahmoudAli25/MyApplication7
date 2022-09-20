95fd1facacba585e391088890197f8d30b4f6771
razi <razi@mograbi.net>
2022-09-20 14:47:23 +0300

    Merge remote-tracking branch 'github/master'

--- app/src/main/java/mahmoudali/myapplication/SignUp.java
+++ app/src/main/java/mahmoudali/myapplication/SignUp.java
@@ -3,6 +3,7 @@ package mahmoudali.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -25,6 +26,21 @@ public class SignUp extends AppCompatActivity {
         ETsignUP=findViewById(R.id.ETsignUP);
         ETpasUp=findViewById(R.id.ETpasUp);
         ETCON=findViewById(R.id.ETCON);
+
+     BTup.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             checkAndSave();
+         }
+     });
+
+    }
+
+    private void checkAndSave()
+    {
+        String email=ETsignUP.getText().toString();
+        String ReEmail=ETCON.getText().toString();
+        String passw=ETpasUp.getText().toString();
 
 
     }
