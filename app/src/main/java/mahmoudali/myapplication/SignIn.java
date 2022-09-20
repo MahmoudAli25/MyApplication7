f42ea2842605a92dbd99d26f4b865adeaaff6535
razi <razi@mograbi.net>
2022-09-20 08:19:38 +0300

    school

--- app/src/main/java/mahmoudali/myapplication/SignIn.java
+++ app/src/main/java/mahmoudali/myapplication/SignIn.java
@@ -1,17 +1,23 @@
 package mahmoudali.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignIn extends AppCompatActivity {
-    private TextInputEditText EtEmail;
-    private TextInputEditText EtPass;
+    TextInputEditText EtEmail;
+    TextInputEditText EtPass;
     private Button button5;
     private Button button6;
 
@@ -32,5 +38,61 @@ public class SignIn extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAndSave();
+            }
+        });
+    }
+
+    private void checkAndSave()
+    {
+        String email=EtEmail.getText().toString();
+        String passw=EtPass.getText().toString();
+        boolean isOK=true;
+        if(email.length()==0)
+        {
+            EtEmail.setError("enter your email");
+            isOK=false;
+        }
+        if(passw.length()==0)
+        {
+            EtPass.setError("enter your password");
+            isOK=false;
+        }
+        if(email.indexOf("@")<=0)
+        {
+            EtEmail.setError("wrong email");
+            isOK=false;
+        }
+        if(passw.length()<7)
+        {
+            EtPass.setError("password at least 7 character");
+            isOK=false;
+        }
+        if(isOK)
+        {
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            auth.signInWithEmailAndPassword(email,passw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task)
+                {
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(SignIn.this, "Successful", Toast.LENGTH_SHORT).show();
+                        Intent G=new Intent(SignIn.this,MainActivity.class);
+                        startActivity(G);
+                        finish();
+                    }
+                    else
+                    {
+                        Toast.makeText(SignIn.this, "Not Successful"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+
+        }
     }
 }
